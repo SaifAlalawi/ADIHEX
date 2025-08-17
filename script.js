b373f0a3972ac047a509eba8b646a4e4dec7fc9c
SaifAlalawi <s.a.s.alalawi@hotmail.com>
2025-08-17 17:53:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ finishBtn.addEventListener("click", ()=>{
   thankYou.innerHTML = `
     <p class="bold" style="font-size:1.4rem; margin-bottom:0.5rem; color:#065f46;">شكرًا لك على التسجيل!</p>
     <p style="color:#065f46; font-size:1rem;">
-      أهلًا وسهلًا بك معرض أبوظبي للصيد والفروسية 2025. نتمنى لك تجربة ممتعة وشيقة!
+      أهلًا وسهلًا بك في معرض أبوظبي الدولي للصيد والفروسية 2025. نتمنى لك تجربة ممتعة وشيقة!
     </p>
   `;
 
