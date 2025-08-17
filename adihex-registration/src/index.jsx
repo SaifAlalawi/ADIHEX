91ea3ba4545a5f9a3840e6db91cc08fe5dceb787
SaifAlalawi <s.a.s.alalawi@hotmail.com>
2025-08-17 15:11:06 +0400

    Update and rename index.js to index.jsx

--- adihex-registration/src/index.jsx
+++ adihex-registration/src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import RegistrationForm from "./RegistrationForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RegistrationForm />
+    <App />
   </React.StrictMode>
 );
